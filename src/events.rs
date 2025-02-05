ed08837f0a6452ea762a6b823ebf4b22633af49f
Isax03 <isaia.tonini@gmail.com>
2025-02-05 09:19:04 +0100

    added latency to HostMessageSent

--- src/events.rs
+++ src/events.rs
@@ -1,10 +1,11 @@
-use wg_2024::packet::Packet;
 use crate::chat::message::HostMessage;
 use crate::PacketHeader;
+use std::time::Duration;
+use wg_2024::packet::Packet;
 
 #[derive(Debug, Clone)]
 pub enum HostEvent {
-    HostMessageSent(HostMessage),
+    HostMessageSent(HostMessage, Duration),
     PacketSent(PacketHeader),
     ControllerShortcut(Packet),
 }
