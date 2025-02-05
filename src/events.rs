5d45415d4996563c73eb55c810882e0beae2fd07
Riccardo Benevelli <rickybenevelli@gmail.com>
2025-02-05 12:27:08 +0100

    feat: destination of HostMessageSent

--- src/events.rs
+++ src/events.rs
@@ -1,11 +1,12 @@
 use crate::chat::message::HostMessage;
 use crate::PacketHeader;
 use std::time::Duration;
+use wg_2024::network::NodeId;
 use wg_2024::packet::Packet;
 
 #[derive(Debug, Clone)]
 pub enum HostEvent {
-    HostMessageSent(HostMessage, Duration),
+    HostMessageSent(NodeId, HostMessage, Duration),
     PacketSent(PacketHeader),
     ControllerShortcut(Packet),
 }
